2af9b849ab75ad5f65f529a7dd6216787309cc47
taro <taro@example.com>
2023-10-31 13:05:12 +0000

    kadai6

---
+++ kadai6.js
@@ -0,0 +1,14 @@
+// 配列の定義
+let fruit = ['apricot', 'strawberry'];
+
+// 配列の先頭に追加
+fruit.unshift('pear');
+
+// 配列の末尾に追加
+fruit.push('grape');
+
+// 配列の末尾を変更
+fruit[3] = 'lime';
+
+// 結果の出力
+console.log(fruit);

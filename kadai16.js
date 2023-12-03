a9b5240648890f233d855201852e1e01acd93404
taro <taro@example.com>
2023-12-03 08:13:40 +0000

    kadai16修正

--- kadai16.js
+++ kadai16.js
@@ -2,7 +2,7 @@ $(function () {
   $('#btn').on('click', function() {
     // ここに処理を記述してください
     $('#msg').text('ボタンが押されました');
-    $('#msg').css('color','blue');
+    $('#msg').addClass('class_01');
   });
 });
  
